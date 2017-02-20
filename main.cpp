cccb5331992288f4fd097395a32d8600639c3ff0
Turupawn <ahmed.hn.43@gmail.com>
2017-02-20 17:41:03 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Test.h"
+#include <iostream>
+using namespace std;
+
+vector<int> makeChange(vector<int> sorted_coins, int amount)
+{
+    vector<int>answer;
+    return answer;
+}
+
+int main ()
+{
+    test();
+    return 0;
+}
